0660841f04bf9254724f378969bf33e61affcdbe
ynzhang <ynzhang@hillinsight.com>
2018-06-26 19:06:12 +0800

    out

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import * as utils from './lib/utils';
+// import * as utils from './lib/utils';
 
-export default {
-    utils : utils,
+// export default {
+//     utils : utils,
+// }
+
+var globalToos = {
+
+
+    sendText : function(name){
+        console.log('hello' + name);
+    }
 }
+exports.globalToos = globalToos;
