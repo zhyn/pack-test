9ba7c99a96bd30620ed01d677aa6836ca7e3e3f6
ynzhang <ynzhang@hillinsight.com>
2018-06-27 16:36:29 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import globalTools from './lib/utils';
-import eppSDK from './lib/clientSDK';
+import * as globalTools from './lib/utils';
+import * as eppSDK from './lib/clientSDK';
 
 
 const mains = {
     globalTools: globalTools,
     eppSDK: eppSDK,
 }
-module.export = mains;
+export default mains;
