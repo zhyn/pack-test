0430403ac3fb12ffadcf84f9a6c3f86536ef6390
ynzhang <ynzhang@hillinsight.com>
2018-07-02 15:05:30 +0800

    add http

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import * as globalTools from './lib/utils';
 import * as eppSDK from './lib/clientSDK';
-
+import * as http from './lib/sdk';
 
 const mains = {
     globalTools: globalTools,
     eppSDK: eppSDK,
+    http: http
 }
 export default mains;
